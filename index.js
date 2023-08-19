d32ec16bcfc637c5b7afe4391bc32294df8c9af7
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-19 03:03:42 +0000

    render color scheme in Javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+let colorArray = ["#F55A5A", "#2B283A", "#FBF3AB", "#AAD1B6", "#A626D3"];
+
+const form = document.getElementById("color-scheme");
+
+function renderScheme() {
+  let colorHtml = "";
+  let codeHtml = "";
+  for (let color of colorArray) {
+    colorHtml += `<div class="color" style="background: ${color}"></div>`;
+    codeHtml += `<span class="hex">${color}</span>`;
+  }
+  document.getElementById("colors").innerHTML = colorHtml + codeHtml;
+}
+
+renderScheme();
+
